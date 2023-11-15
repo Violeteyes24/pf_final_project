2b681dc37cc5faa8b7160a36e2228a7b76aa97e3
Violeteyes24 <gmzach024@gmail.com>
2023-11-15 10:03:51 +0800

    successfully inserted data, problem was this argment, function inside a constructor and forgot to call that function on actionlistener

--- main/src/sign_in_form.java
+++ main/src/sign_in_form.java
@@ -66,21 +66,6 @@ public class sign_in_form extends JFrame {
         add(new JLabel()); // Placeholder
         add(signInButton);
 
-        private void displayUserInfo() {
-            String email_display = email.getText();
-            char[] password = passwordField.getPassword();
-            Sex sexobj = (Sex) sex.getSelectedItem();
-            String location = locationField.getText();
-            String name = nameField.getText();
-            String age = ageField.getText();
-    
-            System.out.println("Email: " + email_display);
-            System.out.println("Password: " + new String(password));
-            System.out.println("Sex: " + sexobj);
-            System.out.println("Location: " + location);
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-        }
         
         // Add action listener to the sign-in button
         signInButton.addActionListener(new ActionListener() {
@@ -89,50 +74,65 @@ public class sign_in_form extends JFrame {
                 JFrame email_validator;
                 email_validator = new JFrame();
                 final String email_var = (String)email.getText();
-                    if (!email_var.equals(email_var) && email_var.equals("@")){
+                if (!email_var.equals(email_var) && email_var.equals("@")){
                     JOptionPane.showMessageDialog(email_validator,"Please enter a valid email");
                     // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            }
-                displayUserInfo();
-            }
-        });
-
-
-    
-    private void insertUserInfo() {
-        try {
-            // Load the JDBC driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            
-            // Establish a connection
-            try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmnt = conn.createStatement();){
-                // Create a prepared statement
-                String useDBSQL = "USE dmid";
-                stmnt.executeUpdate(useDBSQL);
-
-                String sql = "INSERT INTO sign_up (name, age, sex, location, email, passwordField) VALUES (?, ?, ?, ?, ?, ?)";
-                try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-                    // Set values for the prepared statement
-                    preparedStatement.setString(1, nameField.getText());
-                    preparedStatement.setInt(2, Integer.parseInt(ageField.getText())); // age is int
-                    preparedStatement.setString(3, sex.getSelectedItem().toString());
-                    preparedStatement.setString(4, locationField.getText());
-                    preparedStatement.setString(5, email.getText());
-                    preparedStatement.setString(6, new String(passwordField.getPassword()));
-                    
-                    // Execute the statement
-                    preparedStatement.executeUpdate();
-                    
-                    JOptionPane.showMessageDialog(this, "User information inserted successfully!");
                 }
+                displayUserInfo();
+                insertUserInfo();
+                // caller of 2 functions after pressing sign in button
             }
-        } catch (ClassNotFoundException | SQLException ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(this, "Error inserting user information.");
+            private void displayUserInfo() {
+                String email_display = email.getText();
+                char[] password = passwordField.getPassword();
+                Sex sexobj = (Sex) sex.getSelectedItem();
+                String location = locationField.getText();
+                String name = nameField.getText();
+                String age = ageField.getText();
+        
+                System.out.println("Email: " + email_display);
+                System.out.println("Password: " + new String(password));
+                System.out.println("Sex: " + sexobj);
+                System.out.println("Location: " + location);
+                System.out.println("Name: " + name);
+                System.out.println("Age: " + age);
+            }
+            private void insertUserInfo() {
+                try {
+                    // Load the JDBC driver
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    
+                    // Establish a connection
+                    try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    Statement stmnt = conn.createStatement();){
+                        // Create a prepared statement
+                        String useDBSQL = "USE dmid";
+                        stmnt.executeUpdate(useDBSQL);
+        
+                        String sql = "INSERT INTO sign_up (name, age, sex, location, email, passwordField) VALUES (?, ?, ?, ?, ?, ?)";
+                        try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+                            // Set values for the prepared statement
+                            preparedStatement.setString(1, nameField.getText());
+                            preparedStatement.setInt(2, Integer.parseInt(ageField.getText())); // age is int
+                            preparedStatement.setString(3, sex.getSelectedItem().toString());
+                            preparedStatement.setString(4, locationField.getText());
+                            preparedStatement.setString(5, email.getText());
+                            preparedStatement.setString(6, new String(passwordField.getPassword()));
+                            
+                            // Execute the statement
+                            preparedStatement.executeUpdate();
+                            
+                            JOptionPane.showMessageDialog(sign_in_form.this, "User information inserted successfully!");
+                            // "this" argument was for action listener, putting "sign_in_form.this was to indicate that it refers to the outer class"
+                        }
+                    }
+                } catch (ClassNotFoundException | SQLException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(sign_in_form.this, "Error inserting user information.");
+                }
         }
+        });
     }
-}
 
 public static void main(String[] args) {
     SwingUtilities.invokeLater(new Runnable() {
