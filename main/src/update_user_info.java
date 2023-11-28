cf3843c5493f1fd6548ddfe13508067cb612d0d0
Violeteyes24 <gmzach024@gmail.com>
2023-11-28 19:31:47 +0800

    added a back button and padding of update_user_info

--- main/src/update_user_info.java
+++ main/src/update_user_info.java
@@ -10,12 +10,16 @@ public class update_user_info extends JFrame {
     private JComboBox<String> newSexField;
     private JTextField newLocationField;
     private JPasswordField newPasswordField;
+    private JButton backButton;
 
     public update_user_info(String userEmail) {
         setTitle("Update User Information");
         setSize(700, 400);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        JPanel mainPanel = new JPanel(new GridLayout(8, 2, 10, 1));
+        mainPanel.setBackground(Color.PINK);
 
         // Create components
         JLabel nameLabel = new JLabel("Enter new name:");
@@ -32,22 +36,30 @@ public class update_user_info extends JFrame {
         newPasswordField = new JPasswordField(20);
 
         JButton updateButton = new JButton("Update");
+        JButton backButton = new JButton("<<");
+        backButton.setBounds(100,100,100,80);
 
         // Set up layout
         setLayout(new GridLayout(6, 2, 10, 10));
         getContentPane().setBackground(Color.PINK);
-        add(nameLabel);
-        add(newNameField);
-        add(ageLabel);
-        add(newAgeField);
-        add(sexLabel);
-        add(newSexField);
-        add(locationLabel);
-        add(newLocationField);
-        add(passwordLabel);
-        add(newPasswordField);
-        add(new JLabel()); // Placeholder
-        add(updateButton);
+        mainPanel.add(nameLabel);
+        mainPanel.add(newNameField);
+        mainPanel.add(ageLabel);
+        mainPanel.add(newAgeField);
+        mainPanel.add(sexLabel);
+        mainPanel.add(newSexField);
+        mainPanel.add(locationLabel);
+        mainPanel.add(newLocationField);
+        mainPanel.add(passwordLabel);
+        mainPanel.add(newPasswordField);
+        mainPanel.add(new JLabel()); // Placeholder
+        mainPanel.add(updateButton);
+        mainPanel.add(backButton);
+
+        setContentPane(mainPanel);
+
+        int padding = 40;
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
 
         // Add action listener to the update button
         updateButton.addActionListener(new ActionListener() {
@@ -65,6 +77,20 @@ public class update_user_info extends JFrame {
                 // Close the update window
                 setVisible(false);
                 dispose();
+            }
+        });
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                homepage h = new homepage();
+                
+                setVisible(false); // Close the signup window
+                dispose();  // Release system resources
+                
+                // Open the login window
+                System.out.println("I am on homepage from udpate_user");
+                h.setVisible(true);
             }
         });
     }
